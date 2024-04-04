a15ee3ffa0b8e517b8a5826b0c6ba55fb1db6494
Maira Naeem <163671644+mairanoor412@users.noreply.github.com>
2024-04-04 00:05:30 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    const todosanswer = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: "what would you like to add in you todos ?",
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: "would you like to add more in your todos ?",
+            default: "true",
+        }
+    ]);
+    todos.push(todosanswer.firstQuestion);
+    console.log(todos);
+    condition = todosanswer.secondQuestion;
+}
